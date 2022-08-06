6f4b74018dcaf0127c0abe14d5fedc4778569593
Abinash Karmakar <codeAbinash@gmail.com>
2022-08-06 19:21:07 +0530

    Shift + F and space replace search

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@ import debounce from './debounce.js'
 
 highligh()
 
-let searchBox = document.getElementById('searchInput')
+let searchBar = document.getElementById('searchInput')
 let main = document.getElementById('allCodes')
+let windowWidth = window.innerWidth;
 
 
 const fileExtensions = ['.c', '.cpp', '.js', '.py', '.java', '.txt']
@@ -37,32 +38,32 @@ fileExtensions.forEach(fileExtension => {
 })
 
 
-searchBox.addEventListener("input", debounce(() => { searchFile() }, 500, false))
+searchBar.addEventListener("input", debounce(() => { searchFile() }, 500, false))
 function searchFile() {
-    let searchStr = searchBox.value.trim().toLowerCase()
+    let searchStr = searchBar.value.trim().toLowerCase()
     if(searchStr.includes('leetcode.com')){
         searchStr = searchStr.replace('https://leetcode.com/problems/','')
         if(searchStr.includes('/'))
             searchStr = searchStr.substring(0,searchStr.indexOf('/'))
     }
+    searchStr = searchStr.split(' ').join('-')
 
     if (!searchStr) {
         main.style.display = 'none'
         return
     }
-
     main.style.display = 'block'
     const fetchLink = './files/problems/' + searchStr
     loadCodes(fetchLink)
 }
 
-searchBox.addEventListener('search', ()=>{
-    searchBox.blur()
+searchBar.addEventListener('search', ()=>{
+    searchBar.blur()
 })
-searchBox.onkeydown = (e) => {
+searchBar.onkeydown = (e) => {
     if (e.key === 'Escape') {
         e.preventDefault();
-        searchBox.blur()
+        searchBar.value = ''
     }
 }
 
@@ -85,6 +86,8 @@ function getDomText(text, codeType) {
 
 function loadCodes(fetchLink,) {
     let loadPromises = []
+    let found = false
+
     fileExtensions.forEach((fileExtension) => {
         const codeType = fileExtension.substring(1)
         fetchData(fetchLink + fileExtension)
@@ -103,6 +106,7 @@ function loadCodes(fetchLink,) {
                     }
                     text.then((txt) => {
                         codeDOM[codeType].innerHTML = getDomText(txt, codeType)
+                        found = true
                     })
                 })
             loadPromises.push(promise)
@@ -111,9 +115,35 @@ function loadCodes(fetchLink,) {
 
     Promise.allSettled(loadPromises).then(() => {
         setTimeout(() => { highligh() }, 50);
+        if(found)
+            searchBar.blur()
     })
 }
 
+
+// Shift + F
+let keys = {}
+function keyPress(e) {
+    let { key, type } = e
+    let isKeyDown = (type == 'keydown')
+    keys[key] = isKeyDown;
+    if (isKeyDown && keys.Shift && (keys.f || keys.F)) {
+        let tagNameFocusedElem = document.activeElement.tagName
+        if (tagNameFocusedElem != 'INPUT') {
+            e.preventDefault()
+            searchBar.focus();
+        }
+    }
+    if(isKeyDown == false)
+        keys = {}
+}
+document.onkeyup = (e) => {keyPress(e)}
+document.onkeydown = (e) => {keyPress(e)}
+
+
+if (windowWidth >= 900){
+    searchBar.placeholder += " (\" Shift + F \")"
+}
 
 
 // searchBar.addEventListener("input", debounce(() => { filterSearch(searchBar.value) }, 300, false))
