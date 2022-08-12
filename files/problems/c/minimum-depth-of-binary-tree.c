656472da93117e6f8a74654c5953a6efd95e584b
Ananta Karmakar <codeantu@gmail.com>
2022-08-12 18:55:57 +0530

    Create minimum-depth-of-binary-tree.c

---
+++ files/problems/c/minimum-depth-of-binary-tree.c
@@ -0,0 +1,43 @@
+int traverse(struct TreeNode* root,int i) {
+
+    int x,y;
+
+    i++;
+
+    x=i;
+
+    y=i;
+
+    if(root->left && root->right){
+
+        x=traverse(root->left,i);
+
+        y=traverse(root->right,i);
+
+    return x>y?y:x;
+
+    }
+
+    if(root->left)
+
+        x=traverse(root->left,i);
+
+    if(root->right)
+
+        y=traverse(root->right,i);
+
+    return x>y?x:y;
+
+}
+
+int minDepth(struct TreeNode* root) {
+
+    int i=0;
+
+    if(!root)
+
+        return 0;
+
+    return traverse(root,i);
+
+}
