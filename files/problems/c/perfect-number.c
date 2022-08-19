01f8cd29184c60d88e09322edc27d64848b771ba
Abinash Karmakar <codeAbinash@gmail.com>
2022-08-19 20:50:47 +0530

    added perfect-number in c and cpp

---
+++ files/problems/c/perfect-number.c
@@ -0,0 +1,8 @@
+// by @codeAbinash
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+
+bool checkPerfectNumber(int num) {
+    return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336;
+}
---
+++ files/problems/cpp/perfect-number.cpp
@@ -0,0 +1,11 @@
+// by @codeAbinash
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+class Solution {
+   public:
+    bool checkPerfectNumber(int num) {
+        return num == 6 || num == 28 || num == 496 || num == 8128 ||
+               num == 33550336;
+    }
+};
