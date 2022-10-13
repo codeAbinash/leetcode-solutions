ee8bd4b211a08bb7d127035a83020506322a77fd
Abinash <codeAbinash@gmail.com>
2022-10-13 19:12:14 +0530

    cpp

--- files/problems/cpp/subsets.cpp
+++ files/problems/cpp/subsets.cpp
@@ -20,3 +20,22 @@ class Solution {
         return allSubsets;
     }
 };
+
+// by @codeAbinash
+// Using bit masking
+
+class Solution {
+   public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int subsetCount = 1 << n;
+        vector<vector<int>> allSubsets;
+        for (int mask = 0; mask < subsetCount; mask++) {
+            vector<int> subset;
+            for (int i = 0; i < n; i++)
+                if ((mask & (1 << i)) != 0) subset.push_back(nums[i]);
+            allSubsets.push_back(subset);
+        }
+        return allSubsets;
+    }
+};
