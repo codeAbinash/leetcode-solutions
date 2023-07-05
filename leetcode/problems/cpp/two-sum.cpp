58b4d16a9de345a031721c88a8fab42033e3c243
Ananta Karmakar <codeAntu@gmail.com>
2023-07-05 21:53:26 +0530

    added two sum

---
+++ leetcode/problems/cpp/two-sum.cpp
@@ -0,0 +1,21 @@
+// by @codeAntu 
+// time complexity: O(n)
+// space complexity: O(n)
+
+class Solution {
+public:
+   vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map <int, int > map;
+      int rem;
+
+      for (int i = 0; i < nums.size(); i++)
+      {
+         rem = target - nums[i];
+         if (map.find(rem) != map.end())
+            return  {map[rem] , i};
+         else
+            map[nums[i]] = i;
+      }
+      return { -1, -1 };
+   }
+};
