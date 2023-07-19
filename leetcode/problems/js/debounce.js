4c87d97379a68dad49a95b8b452959a5af39d558
Abinash Karmakar <codeAbinash@gmail.com>
2023-07-19 09:17:20 +0530

    Added some code

--- leetcode/problems/js/debounce.js
+++ leetcode/problems/js/debounce.js
@@ -1,9 +1,23 @@
 // by @codeAbinash
+// Time : O(1)
+// Space : O(1)
 
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
 var debounce = function (fn, t) {
-   let timer = null
-   return function (...args) {
-      clearTimeout(timer)
-      timer = setTimeout(fn, t, ...args)
-   }
-};
+    let timer
+    return function (...args) {
+        timer && clearTimeout(timer)
+        timer = setTimeout(() => fn(...args), t)
+    }
+}
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
