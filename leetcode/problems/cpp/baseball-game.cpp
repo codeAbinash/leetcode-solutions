818ecb08567e26f05649d5e000cfeecf6c0c96d4
Abinash Karmakar <codeAbinash@gmail.com>
2023-07-31 20:25:36 +0530

    Some code

--- leetcode/problems/cpp/baseball-game.cpp
+++ leetcode/problems/cpp/baseball-game.cpp
@@ -1,31 +1,32 @@
 // by @codeAbinash
-// Time Complexity : O(n)
-// Space Complexity : O(n)
+// Time : O(n)
+// Space : O(1)
+
+#include "string"
+#include "vector"
+using namespace std;
+
 
 class Solution {
-   public:
-    int calPoints(vector<string>& operations) {
-        stack<int> record;
-        int score = 0;
-        for (auto& op : operations) {
-            if (op == "C") {
-                record.pop();
-            } else if (op == "D") {
-                record.push(record.top() * 2);
-            } else if (op == "+") {
-                int last = record.top();
-                record.pop();
-                int p_last = record.top();
-                record.push(last);
-                record.push(last + p_last);
-            } else {
-                record.push(stoi(op));
-            }
-        }
-        while (!record.empty()) {
-            score += record.top();
-            record.pop();
-        }
-        return score;
-    }
+public:
+   int calPoints(vector<string>& operations) {
+      vector<int> scores;
+      int sum = 0, len = operations.size();
+      for (int i = 0; i < len; i++) {
+         if (operations[i] == "C") {
+            sum -= scores.back();
+            scores.pop_back();
+         } else if (operations[i] == "D") {
+            scores.push_back(scores.back() * 2);
+            sum += scores.back();
+         } else if (operations[i] == "+") {
+            scores.push_back(scores.back() + scores[scores.size() - 2]);
+            sum += scores.back();
+         } else {
+            scores.push_back(stoi(operations[i]));
+            sum += scores.back();
+         }
+      }
+      return sum;
+   }
 };
