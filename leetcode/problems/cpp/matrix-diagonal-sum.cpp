f3002442b838ec639756945c253a4e817e051038
Abinash Karmakar <codeAbinash@gmail.com>
2023-08-01 20:43:12 +0530

    Added some code

---
+++ leetcode/problems/cpp/matrix-diagonal-sum.cpp
@@ -0,0 +1,22 @@
+// by @codeAbinash
+// Time : O(n)
+// Space : O(1)
+
+#include "vector"
+using namespace std;
+
+class Solution {
+public:
+   int diagonalSum(vector<vector<int>>& mat) {
+      int n = mat.size(), sum = 0;
+      int mid = n >> 1;
+
+      for (int i = 0; i < n; i++) {
+         sum += mat[i][i];
+         sum += mat[n - i - 1][i];
+      }
+
+      if (n & 1) sum -= mat[mid][mid];
+      return sum;
+   }
+};
