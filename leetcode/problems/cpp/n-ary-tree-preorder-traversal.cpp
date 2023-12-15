7e153460ed06cf26f1a3f79769991e303026a419
Abinash Karmakar <codeAbinash@gmail.com>
2023-12-15 23:22:00 +0530

    Refactor n-ary tree preorder traversal implementation

--- leetcode/problems/cpp/n-ary-tree-preorder-traversal.cpp
+++ leetcode/problems/cpp/n-ary-tree-preorder-traversal.cpp
@@ -2,38 +2,59 @@
 // Time : O(n)
 // Space : O(n)
 
+#include "stack"
 #include "vector"
+
 using namespace std;
 
 class Node {
-  public:
-   int val;
-   vector<Node*> children;
+public:
+  int val;
+  vector<Node *> children;
 
-   Node() {}
+  Node() {}
 
-   Node(int _val) {
-      val = _val;
-   }
+  Node(int _val) { val = _val; }
 
-   Node(int _val, vector<Node*> _children) {
-      val = _val;
-      children = _children;
-   }
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
 };
 
 class Solution {
-   void dfs(Node* root, vector<int>& result) {
-      if (root == nullptr) return;
-      result.push_back(root->val);
-      for (Node* child : root->children)
-         dfs(child, result);
-   }
-
-  public:
-   vector<int> preorder(Node* root) {
-      vector<int> result;
-      dfs(root, result);
+public:
+  vector<int> preorder(Node *root) {
+    vector<int> result;
+    if (!root)
       return result;
-   }
+
+    stack<Node *> s;
+    s.push(root);
+    while (s.size()) {
+      Node *node = s.top();
+      s.pop();
+      result.push_back(node->val);
+      for (int i = node->children.size() - 1; i >= 0; i--)
+        s.push(node->children[i]);
+    }
+
+    return result;
+  }
+};
+
+class Solution {
+public:
+  vector<int> result;
+  void ans(Node *root) {
+    if (!root)
+      return;
+    result.push_back(root->val);
+    for (int i = 0; i < root->children.size(); i++)
+      ans(root->children[i]);
+  }
+  vector<int> preorder(Node *root) {
+    ans(root);
+    return result;
+  }
 };
