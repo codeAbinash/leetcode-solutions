425a5e077bd2985e3aaeb247ae335481f506cbd4
Abinash Karmakar <codeAbinash@gmail.com>
2023-12-17 23:35:02 +0530

    Added clang format and some solutions

--- leetcode/problems/cpp/number-of-students-unable-to-eat-lunch.cpp
+++ leetcode/problems/cpp/number-of-students-unable-to-eat-lunch.cpp
@@ -1,26 +1,23 @@
-// by @codeAbinash
-// Time Complexity : O(n)
-// Space Complexity : O(n)
+#include "deque"
+#include "vector"
+
+using namespace std;
 
 class Solution {
-   public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        deque<int> q(students.begin(), students.end());
-        int i = 0;
-        int count = 0;
-        while (!q.empty()) {
-            int current = q.front();
-            q.pop_front();
-            if (current != sandwiches[i]) {
-                count++;
-                q.push_back(current);
-            } else {
-                count = 0;
-                i++;
-            }
-
-            if (count == q.size()) break;
-        }
-        return q.size();
-    }
+ public:
+   int countStudents(vector<int>& students, vector<int>& sandwiches) {
+      deque<int> q(students.begin(), students.end());
+      int i = 0, j = 0;
+      while (q.size() != 0 && j != q.size()) {
+         if (q.front() != sandwiches[i]) {
+            q.push_back(q.front());
+            j++;
+         } else {
+            j = 0;
+            i++;
+         }
+         q.pop_front();
+      }
+      return q.size();
+   }
 };
