8538b0bb164ae70418e9edb95aa26207ccac9155
Abinash Karmakar <codeAbinash@gmail.com>
2024-06-23 12:17:02 +0530

    Add hello world in rust

---
+++ leetcode/problems/rs/hello-world.rs
@@ -0,0 +1,13 @@
+// A simple hello world program in Rust
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+// Search for Leetcode Solutions 
+// Paste the link or the name of the problem 
+// 👆🏻 in the Search Bar
+// Hit enter or click the search button
+// The solution will be displayed in this section
